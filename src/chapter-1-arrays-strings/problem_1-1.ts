3dccde94c257c1dca3dd2529d53c820e0a7e6869
Suncerie Daye <sunceried@gmail.com>
2021-10-13 14:23:23 -0700

    Problem 1.1 complete

--- src/chapter-1-arrays-strings/problem_1-1.ts
+++ src/chapter-1-arrays-strings/problem_1-1.ts
@@ -1,3 +1,12 @@
 export const allUniqueCharacters = (word:string): boolean => {
+    let map : {[index: string]: number} = {}; //must have an index signature if using variable to index object
+    for(let char of word.toLocaleLowerCase()){
+        if(map[char]) return false;
+        map[char] = 1;
+    }
     return true;
 }
+
+const result:boolean = allUniqueCharacters('Suncy');
+
+console.log(result);
