1f3a27fcf681c7c373e2ebba30c76344717f75ca
Suncerie Daye <sunceried@gmail.com>
2021-10-13 14:30:37 -0700

    Questions added

--- src/chapter-1-arrays-strings/problem_1-1.ts
+++ src/chapter-1-arrays-strings/problem_1-1.ts
@@ -1,3 +1,4 @@
+//Question: Implement an algorithm to determine if a string has all unique caracters. What if you cannot use additional data structures?
 export const allUniqueCharacters = (word:string): boolean => {
     let map : {[index: string]: number} = {}; //must have an index signature if using variable to index object
     for(let char of word.toLocaleLowerCase()){
